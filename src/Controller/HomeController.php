aa7b1c78852f9402257ba63e6098a6f770599041
karhel <karheltmarr@hotmail.fr>
2025-03-25 11:04:33 +0100

    Quote Generator (with string)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\QuoteGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(QuoteGenerator $quoteGenerator): Response
     {
+        $quote = $quoteGenerator->getRandomQuote();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'quote' => $quote
         ]);
     }
 }
---
+++ src/Service/QuoteGenerator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+final class QuoteGenerator
+{
+    public function getRandomQuote(): string
+    {
+        return "Vaut mieux taper toujours les mêmes, il y en a moins qui râlent";
+    }
+}
