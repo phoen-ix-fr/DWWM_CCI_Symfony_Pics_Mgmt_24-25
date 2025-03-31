04344305440584194401a3a58c3c38e4d0aadbaf
karhel <karheltmarr@hotmail.fr>
2025-03-31 09:51:22 +0200

    Edit BootstrapCard to allow 2 or more buttons

--- src/Twig/Components/BootstrapCard.php
+++ src/Twig/Components/BootstrapCard.php
@@ -9,8 +9,6 @@ final class BootstrapCard
 {
     public string $title;       //< Titre de la tuile (card)
     public string $imgSrc;      //< L'URL qui sera dans le src de l'image
-    public string $btnLink;     //< Lien au clic sur le bouton du footer de la card
-    public string $btnLabel;    //< Le texte du bouton du footer de la card
 
     
 }
