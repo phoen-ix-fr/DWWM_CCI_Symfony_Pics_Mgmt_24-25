5a8659f8395cbe5084e98b0a600a10e472d36587
karhel <karheltmarr@hotmail.fr>
2025-04-11 09:59:01 +0200

    update fixtures

--- src/DataFixtures/EventFixture.php
+++ src/DataFixtures/EventFixture.php
@@ -12,17 +12,25 @@ class EventFixture extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
+
+        $userRepository = $manager->getRepository(User::class);
+        $users = $userRepository->findAll();
+
+        foreach($users as $user)
+        {   
         
-        // Je génère entre 5 et 10 évènements
-        $nbEvents = $faker->numberBetween(5, 10);
+            // Je génère entre 5 et 10 évènements
+            $nbEvents = $faker->numberBetween(5, 10);
 
-        for($i = $nbEvents; $i >= 0; $i--) {
+            for($i = $nbEvents; $i >= 0; $i--) {
 
-            $event = new Event();
-            $event->setTitle($faker->sentence(4, true))
-                ->setDate($faker->dateTimeBetween('-1 year'));
-    
-            $manager->persist($event);
+                $event = new Event();
+                $event->setTitle($faker->sentence(4, true))
+                    ->setDate($faker->dateTimeBetween('-1 year'))
+                    ->setCreatedBy($user);
+        
+                $manager->persist($event);
+            }
         }
 
         $manager->flush();
--- src/DataFixtures/PictureFixture.php
+++ src/DataFixtures/PictureFixture.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use DateTime;
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Event;
 use App\Entity\Picture;
 use Doctrine\Persistence\ObjectManager;
@@ -14,16 +15,23 @@ class PictureFixture extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
-        
-        $nbPictures = $faker->numberBetween(5, 10);
 
-        for($i = 0; $i < $nbPictures; $i++) {
-            $picture = new Picture();
-            $picture->setDescription($faker->sentence(4, true))
-                ->setDate($faker->dateTimeBetween('-1 year'))
-                ->setFilename($faker->word() . ".jpg");
-    
-            $manager->persist($picture);
+        $userRepository = $manager->getRepository(User::class);
+        $users = $userRepository->findAll();
+
+        foreach($users as $user)
+        {        
+            $nbPictures = $faker->numberBetween(5, 10);
+
+            for($i = 0; $i < $nbPictures; $i++) {
+                $picture = new Picture();
+                $picture->setDescription($faker->sentence(4, true))
+                    ->setDate($faker->dateTimeBetween('-1 year'))
+                    ->setFilename($faker->word() . ".jpg")
+                    ->setCreatedBy($user);
+        
+                $manager->persist($picture);
+            }
         }
 
         $eventRepository = $manager->getRepository(Event::class);
@@ -39,10 +47,12 @@ class PictureFixture extends Fixture
                 $picture->setDescription($faker->sentence(4, true))
                     ->setDate($faker->dateTimeBetween('-1 year'))
                     ->setFilename($faker->word() . ".jpg")
-                    ->setEvent($event);
+                    ->setEvent($event)
+                    ->setCreatedBy($event->getCreatedBy());
         
                 $manager->persist($picture);
             }
+        
         }
 
         $manager->flush();
